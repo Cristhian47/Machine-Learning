beb5e146000dd53aa575e5780982ad7b96ab4ce9
Cristhian1620 <cristian.grajales@alternova.com>
2023-02-16 19:23:10 -0500

    New requirements and data in dvc files

--- code/0_filterdataset.py
+++ code/0_filterdataset.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Read in the data
-df = pd.read_csv('data/dataset/Salary_Data.csv')
-print(df.head())
-plt.plot(df('YearsExperience'), df('Salary'))
+#df = pd.read_csv('data/dataset/Salary_Data.csv')
+#print(df.head())
+#plt.plot(df('YearsExperience'), df('Salary'))
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
